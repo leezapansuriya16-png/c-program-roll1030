7084893bd7b9345b6cf5d1c2ee79f9760ef5c080
leezapansuriya16-png <leezapansuriya16@gmail.com>
2025-10-07 12:58:01 +0530

    first

---
+++ ass_1030_1.c
@@ -0,0 +1,12 @@
+//convert kilometers to miles
+#include<stdio.h>
+int main()
+{
+  float kilometers;
+  float miles;
+    printf("enter distance in kilometers:");
+    scanf("%f",&kilometers);
+    miles= kilometers/1.609344;
+    printf("%.2f kilometers is equal to %.2f miles.\n",kilometers,miles);
+    return 0;
+}
